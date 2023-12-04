de45b50c932dd3054fd6a84594c60a91c74fc4d6
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2023-12-04 11:31:38 +0700

    Update String.h

--- String.h
+++ String.h
@@ -8,10 +8,12 @@
 #include<time.h>
 #include<string.h>
 #define MaxSize 100
+#define Col 20
 void menu();
 
-void Input(char*& s1, char*& s2);
-void Output(char* s1, char* s2);
+void Input( char*& s2);
+void Output(char* s);
+void OutputArrayString(char **s, int &m);
 
 void deleteEndline(char*& s);
 
@@ -20,9 +22,18 @@ void getValueIndex(char*& s, int& n);
 void Upper(char*& s, int& n);
 void upperName(char*& s, int& n);
 
-void readFile(const char* filename, char* s1, char* s2);
+void init(char **s, int &m);
+
+void readFile(const char* filename, char** &s, int &m);
 void writeFile(const char* filename, char* s1, char* s2);
 
 int checkS1String(char*& s1);
-void insertFirstName(char* s1, char* s2, int startPos);
+void realForm(char *&s);
+void deleteExtraWhiteSpace(char *&s);
+void devideFisrtSecondName(char *&s);
+int findName(char *&s, char *s1);
+int checkOpposite(char *&s);
+int getIndexS1InS(char *&s, char *&s1);
+void insertStringIndex(char* str, char* strInsert, int index);
+void deleleAllChar(char *&s, char c);
 #endif
